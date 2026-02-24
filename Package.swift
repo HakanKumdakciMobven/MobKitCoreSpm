6cb6318b55b7ee3f65821a09989146440cd5e339
Hakan <hakan.kumdakci@mobven.com>
2026-02-24 15:50:15 +0300

    initialized spm

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "MobKitCore",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "MobKitCore",
+            targets: ["MobKitCore"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MobKitCore",
+            path: "MobKitCore.xcframework"
+        ),
+    ]
+)
